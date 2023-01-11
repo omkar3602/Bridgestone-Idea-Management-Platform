98be3da7c79c458f147b2792434305e1b54e158e
omkar3602 <73201152+omkar3602@users.noreply.github.com>
2023-01-11 11:53:41 +0530

    Add database models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,20 +1,20 @@
-# from django.db import models
+from django.db import models
+from userauth.models import Account
 
 # # Create your models here.
-# class Subject(models.Model):
-#     name = models.CharField(max_length=20, unique=True)
-#     tagline = models.CharField(max_length=50)
-#     image = models.ImageField(upload_to ='subjects/')
+class BusinessUnit(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    idea_champion = models.ForeignKey(Account, on_delete=models.CASCADE)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
-# class Topic(models.Model):
-#     name = models.CharField(max_length=50)
-#     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
-#     link = models.CharField(max_length=50)
-#     qr_code = models.ImageField(upload_to ='topics/qr_codes/')
-#     image = models.ImageField(upload_to ='topics/')
+class Submission(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)
+    business_unit = models.ForeignKey(BusinessUnit, on_delete=models.CASCADE)
+    ideator = models.ForeignKey(Account, on_delete=models.CASCADE)
+    # attachments = models.ImageField(upload_to ='submission_attachments/')
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
