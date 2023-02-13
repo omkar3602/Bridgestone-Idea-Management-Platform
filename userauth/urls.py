04a8bdbff6350ada9af0a5e857bf7fac5779587e
omkar3602 <73201152+omkar3602@users.noreply.github.com>
2023-02-14 01:25:24 +0530

    Add forgot password backend

--- userauth/urls.py
+++ userauth/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('signup_ic/', views.signup_IC, name='signup_IC'),
     path('logout/', views.logout_user, name='logout'),
-    path('forgot_password/', views.forgot_password, name='forgot_password'),
 ]
--- userauth/views.py
+++ userauth/views.py
@@ -89,5 +89,38 @@ def logout_user(request):
         return redirect('home')
     return redirect('home')
 
-def forgot_password(request):
-    return render(request, 'userauth/forgot_password.html')
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.template.loader import render_to_string
+from django.db.models.query_utils import Q
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+
+def password_reset_request(request):
+    if request.method == 'POST':
+        data = request.POST
+        email = data['email']
+        associated_users = Account.objects.filter(Q(email=email))
+        if associated_users.exists():
+            for user in associated_users:
+                subject = "Password Reset Requested at Bridgestone Innovation Garage"
+                email_template_name = "userauth/password_reset/password_reset_email.txt"
+                c = {
+                "email":user.email,
+                'domain':'127.0.0.1:8000',
+                'site_name': 'Bridgestone Innovation Garage',
+                "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                "user": user,
+                'token': default_token_generator.make_token(user),
+                'protocol': 'http',
+                }
+                email = render_to_string(email_template_name, c)
+                try:
+                    send_mail(subject, email, 'omkarj3602@gmail.com' , [user.email], fail_silently=False)
+                except BadHeaderError:
+                    return HttpResponse('Invalid header found.')
+                return redirect ("/password_reset/done/")
+    return render(request=request, template_name="userauth/password_reset/password_reset.html")
