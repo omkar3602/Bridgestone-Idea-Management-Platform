3ac7840a9d2d69b869b251ed379d3fdec936633b
omkar3602 <73201152+omkar3602@users.noreply.github.com>
2023-02-21 19:40:13 +0530

    Edit settings.py

--- bridgestone/settings.py
+++ bridgestone/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oys#sof#-g(t-%y0iiaifwh2j)92p8vl)h+!0_qt!##6dox(wn'
-
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -142,8 +145,8 @@ MEDIA_URL = '/media/'
     # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = "omkarj3602@gmail.com"
-EMAIL_HOST_PASSWORD = "klpvxjxhdcapkllk"
+EMAIL_HOST_USER = str(os.getenv('ADMIN_EMAIL'))
+EMAIL_HOST_PASSWORD = str(os.getenv('PASSWORD'))
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_EMAIL_FROM = f"Bridgestone Innovation Garage <omkarj3602@gmail.com>"
+DEFAULT_EMAIL_FROM = f"Bridgestone Innovation Garage <{str(os.getenv('ADMIN_EMAIL'))}>"
